a8cbd92cf24553895e0612e3e3b344ef62031c4e
Adrian Sims <asims@parkeon.com>
2018-04-19 18:17:47 +0100

    Fix bugs.

---
+++ QsGen/PopularList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TlvSerialise;
+namespace QsGen
+{
+    internal class PopularList : TlvSerialisable
+    {
+        [Tlv(TlvTypes.UInt, TlvTags.ID_POP_NUMERO_VERSION)]
+        public int Version { get; set; }
+
+        [Tlv(TlvTypes.String, TlvTags.ID_POP_IAP)]
+        public string TVMId { get; set; }
+
+        [Tlv(TlvTypes.Array, TlvTags.ID_POP_POPULAR)]
+        public List<PopularDestination> PopularTickets { get; set;}
+    }
+}
